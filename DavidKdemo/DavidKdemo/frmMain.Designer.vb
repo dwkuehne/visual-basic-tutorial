8e010bd04c56a04f94d1964ec275404b26f8b97a
Dave <david.kuehne@tstc.edu>
2020-06-04 19:35:06 -0500

    First Demonstration

---
+++ DavidKdemo/DavidKdemo/frmMain.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.pbxPicture = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtCelcius = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblDegrees = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        CType(Me.pbxPicture, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(600, 280)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(98, 34)
+        Me.btnExit.TabIndex = 0
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(147, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(551, 126)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = resources.GetString("Label1.Text")
+        '
+        'pbxPicture
+        '
+        Me.pbxPicture.Image = Global.DavidKdemo.My.Resources.Resources.warm
+        Me.pbxPicture.Location = New System.Drawing.Point(12, 12)
+        Me.pbxPicture.Name = "pbxPicture"
+        Me.pbxPicture.Size = New System.Drawing.Size(128, 127)
+        Me.pbxPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbxPicture.TabIndex = 1
+        Me.pbxPicture.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 183)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(149, 24)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Degrees Celcius"
+        '
+        'txtCelcius
+        '
+        Me.txtCelcius.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCelcius.Location = New System.Drawing.Point(167, 183)
+        Me.txtCelcius.Name = "txtCelcius"
+        Me.txtCelcius.Size = New System.Drawing.Size(72, 29)
+        Me.txtCelcius.TabIndex = 4
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(255, 177)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(109, 42)
+        Me.btnCalculate.TabIndex = 5
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblDegrees
+        '
+        Me.lblDegrees.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDegrees.Location = New System.Drawing.Point(12, 231)
+        Me.lblDegrees.Name = "lblDegrees"
+        Me.lblDegrees.Size = New System.Drawing.Size(352, 83)
+        Me.lblDegrees.TabIndex = 6
+        Me.lblDegrees.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(370, 177)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(109, 42)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "C&lear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(710, 326)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lblDegrees)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtCelcius)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pbxPicture)
+        Me.Controls.Add(Me.btnExit)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Degrees Converter"
+        CType(Me.pbxPicture, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents pbxPicture As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtCelcius As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblDegrees As Label
+    Friend WithEvents btnClear As Button
+End Class
---
+++ DavidKdemo/DavidKdemo/frmMain.vb
@@ -0,0 +1,56 @@
+﻿'A comment about Celsuis to Fahrenheit calculations
+'The formula: F = (C * 1.8000) + 32.00
+
+'PEMDAS - Parenthesis, Exponents, Multiply or Divide, Add or Subtract
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim dblDegreesF As Double = 0
+        Dim dblDegreesC As Double = 0
+
+        If txtCelcius.Text = String.Empty Then
+            Return
+        End If
+
+        If Char.IsDigit(txtCelcius.Text) Then
+            dblDegreesC = txtCelcius.Text
+            'The formula: F = (C * 1.8000) + 32.00
+            dblDegreesF = dblDegreesC * 1.8 + 32.0
+            lblDegrees.Text = "Degrees in Fahrenheit: " + dblDegreesF.ToString()
+        Else
+            Return
+        End If
+
+        Select Case dblDegreesF
+            Case 212
+                lblDegrees.Text = dblDegreesF.ToString() + " Fahrenheit"
+                lblDegrees.Text += " - That's Boiling!"
+            Case 32
+                lblDegrees.Text = dblDegreesF.ToString() + " Fahrenheit"
+                lblDegrees.Text += " - That's Freezing!"
+            Case Else
+                lblDegrees.Text = "Degrees in Fahrenheit: " + dblDegreesF.ToString()
+        End Select
+
+        If dblDegreesF > 100 Then
+            pbxPicture.Image = My.Resources.hot
+        ElseIf dblDegreesF > 75 Then
+            pbxPicture.Image = My.Resources.warm
+        Else
+            pbxPicture.Image = My.Resources.cold
+        End If
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtCelcius.Clear()
+        txtCelcius.Select()
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtCelcius.Select()
+    End Sub
+End Class
