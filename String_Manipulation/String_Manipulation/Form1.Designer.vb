7a0ad8433bcae48308245850af68c19aa16d6d95
Dave <david.kuehne@tstc.edu>
2020-07-05 22:36:23 -0500

    Added string manipulation lab

---
+++ String_Manipulation/String_Manipulation/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tbxInput = New System.Windows.Forms.TextBox()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(13, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(154, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter some Text:"
+        '
+        'tbxInput
+        '
+        Me.tbxInput.Location = New System.Drawing.Point(173, 31)
+        Me.tbxInput.Name = "tbxInput"
+        Me.tbxInput.Size = New System.Drawing.Size(312, 29)
+        Me.tbxInput.TabIndex = 1
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Location = New System.Drawing.Point(33, 163)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(428, 174)
+        Me.lblOutput.TabIndex = 2
+        Me.lblOutput.Text = "Output goes here"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(352, 66)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(133, 37)
+        Me.btnSubmit.TabIndex = 3
+        Me.btnSubmit.Text = "&Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AcceptButton = Me.btnSubmit
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DimGray
+        Me.ClientSize = New System.Drawing.Size(497, 377)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.tbxInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "String Manipulation"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tbxInput As TextBox
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnSubmit As Button
+End Class
---
+++ String_Manipulation/String_Manipulation/frmMain.vb
@@ -0,0 +1,61 @@
+﻿Public Class frmMain
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Output() 'Calling our Sub
+    End Sub
+    Sub Output()
+        'Update our label text with our function return value
+        lblOutput.Text = Manipulate(tbxInput.Text) 'Pass in our textbox control text
+    End Sub
+
+    Function Manipulate(strString As String) As String
+        Dim strOutput As String = ""
+        Dim intNumOfTotalChars As Integer = 0
+        Dim intNumOfNums As Integer = 0
+        Dim intNumOfWhiteSpace As Integer = 0
+        Dim strOriginal As String = ""
+        Dim strNew As String = ""
+
+        strOriginal = strString 'Store the original
+        intNumOfTotalChars = strString.Length 'Count each character
+
+        'Replace a string, checking the equivalent to all lowercase characters
+        strNew = strOriginal.ToLower.Replace("cat", "Dog") + vbNewLine
+        strOutput = strNew 'Add to our output string
+
+        If strOriginal.Length >= 5 Then
+            strNew = strOriginal.Substring(0, 5) 'Get the first 5 characters and assign to variable
+            strOutput += "The first 5 chars are: " + strNew + vbNewLine 'Append to output string
+        End If
+
+        For index = 1 To strString.Length
+            'Count the numbers in the string
+            If Char.IsDigit(strString(index - 1)) Then
+                intNumOfNums += 1
+            End If
+            'Count the number of whitespaces in the string
+            If Char.IsWhiteSpace(strString(index - 1)) Then
+                intNumOfWhiteSpace += 1
+            End If
+        Next
+
+        'Creating a random number to randomize where we search our text string
+        If strString.Length > 0 Then
+            Dim rand As Random = New Random 'Create a new "Random" object
+
+            'Assign our new variable with the results of our random number
+            'I'm expecting a number from 1 and however long the string is
+            Dim randNum As Integer = rand.Next(1, strString.Length + 1)
+
+            'Append to our output string
+            strOutput += "Random number (" + (randNum).ToString() + ") for string: " +
+                vbNewLine + strString.Substring(0, randNum) + vbNewLine
+        End If
+
+        'Append more to our Output String
+        strOutput += "Number of characters in String: " + intNumOfTotalChars.ToString()
+        strOutput += vbNewLine + "Number of digits in String: " + intNumOfNums.ToString()
+        strOutput += vbNewLine + "Number of whitespaces in String: " + intNumOfWhiteSpace.ToString()
+
+        Return strOutput 'Send the result back to whatever called it
+    End Function
+End Class
